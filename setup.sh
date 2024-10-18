4fdc386d989fe2986761830a0363910bf25a7ceb
Thiago Cavichia dos Santos <166753861+Cavichia-D-Santos@users.noreply.github.com>
2024-10-18 20:15:37 -0300

    Update setup.sh email

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"thiagogamer10000@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
